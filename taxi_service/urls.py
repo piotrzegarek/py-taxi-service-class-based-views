45eea1f0fd35a2ec1e441aa84fe240bbe0bd3b56
Piotr Zegarek <pzegarek@intel471.com>
2025-01-24 07:46:11 +0100

    remove django dev toolbar

--- taxi_service/urls.py
+++ taxi_service/urls.py
@@ -18,11 +18,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from debug_toolbar.toolbar import debug_toolbar_urls
-
 
 urlpatterns = ([
     path("admin/", admin.site.urls),
     path("", include("taxi.urls", namespace="taxi")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    + debug_toolbar_urls())
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
